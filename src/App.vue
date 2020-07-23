475b1ed94beef37533a0c08f3e433590247f1cc3
Alussius Aloy <louierue15@gmail.com>
2020-07-22 22:09:51 -0400

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4 class="bg-primary text-white text-center p-2">
+      {{ name }}'s To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" :key="t.action">
+        <div class="col">{{ t.action }}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">Hide Completed Tasks</label>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input type="text" v-model="newItemText" class="form-control">
+        </div>
+        <div class="col-2">
+          <button @click="addNewTodo" class="btn btn-primary">Add</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "Rue Dewprism",
+      tasks: [],
+      hideCompleted: true,
+      newItemText: ""
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({ action: this.newItemText, done: false });
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+      this.newItemText = "";
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      this.tasks = JSON.parse(data);
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
